fdf2eedf929930897587a5b8d400d0b191943f97
Shoyab-0103 <shaikshoyab@globussoft.in>
2025-04-16 12:55:04 +0530

    Initial commit with .gitignore

---
+++ app.py
@@ -0,0 +1,39 @@
+from dotenv import load_dotenv
+load_dotenv()  # Loading environment variables from .env file
+import streamlit as st
+import os
+import google.generativeai as genai
+from google.api_core.exceptions import NotFound
+
+# Configure the API key for Google Generative AI
+genai.configure(api_key=os.getenv("GENAI_API_KEY"))
+
+
+
+# Function to load gemini pro model and get response
+def get_gemini_response(question):
+    try:
+        model = genai.GenerativeModel("models/gemini-1.5-pro-latest")
+        return model.generate_content(question).text
+    except NotFound:
+        return f"Model not found. Available models: {', '.join(model.name for model in genai.list_models())}"
+    except Exception as e:
+        return f"Error: {e}"
+    
+
+
+# Initialize the Streamlit app
+st.set_page_config(page_title="Q & A Demo")
+
+st.header("Gemini Application")
+
+input_text = st.text_input("Enter your question here: ", key="input")
+
+# Create one submit button
+submit_button = st.button("Ask question")
+
+if submit_button:
+    # Get the response from the model
+    response = get_gemini_response(input_text)
+    st.write(response)
+
